3a16f01206e407ab4b5c19948d154cc6a37b2acb
Vandan Shah <vandanshah17@yahoo.com>
2022-07-19 13:18:19 +0530

    debug error

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -24,14 +24,13 @@ class _HomePageState extends State<HomePage> {
   loadData() async {
     final  catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
-    final products = decodedData["products"];
-    
-
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData).map<Item>((Item) => Item.fromMap(Item)).toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: Text("$name's Catalog App"), 
@@ -39,10 +38,10 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: ListView.builder(
-                itemCount: dummyList.length,
+                itemCount: CatalogModel.items.length,
                 itemBuilder: (context, index) {
                   return ItemWidget(
-                    item: dummyList[index],
+                    item: CatalogModel.items[index],
                   );
                 })),
         drawer: MyDrawer());
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
 class Item {
   final int id;
   final String name;
@@ -6,9 +11,99 @@ class Item {
   final String color;
   final String image;
 
-  Item({required this.id,required this.name, required this.desc, required this.price, required this.color,required  this.image});
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    
+
+    return Item(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      desc: map['desc'] as String,
+      price: map['price'] as num,
+      color: map['color'] as String,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
-class CatalogModel{
-static final items = [
-  Item(id: 1, name: "Iphone 12 Pro",desc: "12th Generation",price: 999, color: "#33505a",image: "https://img1.gadgetsnow.com/gd/images/products/additional/large/G201746_View_1/mobiles/smartphones/apple-iphone-12-pro-128-gb-pacific-blue-6-gb-ram-.jpg"),
-];}
+
+class CatalogModel {
+  static List<Item> items = [
+    Item(
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "12th Generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://img1.gadgetsnow.com/gd/images/products/additional/large/G201746_View_1/mobiles/smartphones/apple-iphone-12-pro-128-gb-pacific-blue-6-gb-ram-.jpg"),
+  ];
+}
