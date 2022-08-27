ff4828e4ba87ed7756e7ff4dcafe0a530aa3154f
Vandan Shah <vandanshah17@yahoo.com>
2022-08-28 02:15:34 +0530

    day 30 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
